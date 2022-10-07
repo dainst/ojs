b1cb13a587c6a89c5087cf147cedf9173ba10db7
Marcel Riedel <Riedel-Marcel@gmx.de>
2022-10-07 12:16:48 +0200

    reset default theme to default specs

--- plugins/themes/default/DefaultThemePlugin.inc.php
+++ plugins/themes/default/DefaultThemePlugin.inc.php
@@ -67,10 +67,6 @@ class DefaultThemePlugin extends ThemePlugin {
 					'value' => 'lora_openSans',
 					'label' => __('plugins.themes.default.option.typography.lora_openSans'),
 				],
-				[
-					'value' => 'cargan_openSans',
-					'label' => __('plugins.themes.default.option.typography.cargan_openSans'),
-				],
 			],
 			'default' => 'notoSans',
 		]);
@@ -128,12 +124,6 @@ class DefaultThemePlugin extends ThemePlugin {
 		} elseif ($this->getOption('typography') == 'lora_openSans') {
 			$this->addStyle('font', 'styles/fonts/lora_openSans.less');
 			$additionalLessVariables[] = '@font: "Open Sans", sans-serif;@font-heading: Lora, serif;';
-
-		// dai-specific: add Cargan font combinded with OpenSans
-
-		} elseif ($this->getOption('typography') == 'cargan_openSans') {
-			$this->addStyle('font', 'styles/fonts/cargan_openSans.less');
-			$additionalLessVariables[] = '@font: "Open Sans", sans-serif;@font-heading: Cargan, serif;';
 		} else {
 			$this->addStyle('font', 'styles/fonts/notoSans.less');
 		}
